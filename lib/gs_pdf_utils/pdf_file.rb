05093e8d1f218e80a85d61c0fa91d7e7863d65e4
Felix Bünemann <buenemann@louis.info>
2012-11-21 19:04:37 +0100

    Suppress gs output, optimize extract_pages
    and remove tempfile for extract page.

--- lib/gs_pdf_utils/pdf_file.rb
+++ lib/gs_pdf_utils/pdf_file.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 module GsPdfUtils
   class PdfFile
+    NULL_DEVICE = RbConfig::CONFIG['host_os'] =~ /mswin|mingw/ ? 'NUL' : '/dev/null'
+
     def initialize(pdf_file, ghostscript_binary = "gs")
       unless GsPdfUtils.is_pdf? pdf_file
         raise BadFileType, "#{pdf_file} does not appear to be a PDF", caller
@@ -18,37 +20,31 @@ module GsPdfUtils
       if page < 1 || page > pages
         raise OutOfBounds, "page #{page} is out of bounds (1..#{pages})", caller
       end
-      begin
-        tempfile = Tempfile.new('PdfUtils')
-        cmd = "#{@gs.shellescape} -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -dFirstPage=#{page} -dLastPage=#{page} -sOutputFile=#{tempfile.to_path.shellescape} #{@file.shellescape}"
-        output = `#{cmd}`
-        if $?.to_i > 0 #|| File.size(tempfile.to_path) == 0
-          raise CommandFailed, "command #{cmd} failed with output: #{output}", caller
-        end
-        if File.size(tempfile.to_path) == 0
-          raise ProcessingError, "extracted page is 0 bytes", caller
-        end
-        FileUtils.cp tempfile.to_path, targetfile
-      ensure
-        tempfile.close!
+      cmd = "#{@gs.shellescape} -q -sDEVICE=pdfwrite -dSAFER -dFirstPage=#{page} -dLastPage=#{page} -o #{targetfile.shellescape} #{@file.shellescape} 2>#{NULL_DEVICE}"
+      output = `#{cmd}`
+      if $?.to_i > 0
+        raise CommandFailed, "command #{cmd} failed with output: #{output}", caller
+      end
+      if File.size(targetfile) == 0
+        raise ProcessingError, "extracted page is 0 bytes", caller
       end
       targetfile
     end
 
     # targetfile_template = "test-%d.pdf"
     def extract_pages(targetfile_template)
-      targetfiles = []
-      (1..pages).each do |page|
-        targetfile = sprintf(targetfile_template, page)
-        targetfiles << extract_page(page, targetfile)
+      cmd = "#{@gs.shellescape} -q -sDEVICE=pdfwrite -dSAFER -o #{targetfile_template.shellescape} #{@file.shellescape} 2>#{NULL_DEVICE}"
+      output = `#{cmd}`
+      if $?.to_i > 0
+        raise CommandFailed, "command #{cmd} failed with output: #{output}", caller
       end
-      targetfiles
+      targetfiles = (1..pages).map {|page| sprintf(targetfile_template, page)}
     end
 
     private
 
     def count_pages
-      cmd = "#{@gs.shellescape} -q -dNODISPLAY -c \"(#{@file.shellescape}) (r) file runpdfbegin pdfpagecount = quit\""
+      cmd = "#{@gs.shellescape} -q -dNODISPLAY -c \"(#{@file.shellescape}) (r) file runpdfbegin pdfpagecount = quit\" 2>#{NULL_DEVICE}"
       output = `#{cmd}`
       if $?.to_i > 0
         raise CommandFailed, "command #{cmd} failed with output: #{output}", caller
